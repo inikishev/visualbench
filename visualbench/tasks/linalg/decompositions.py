dc3ec8213975fb4f7a52db5b62798b534f6d81f5
inikishev <nkshv2@gmail.com>
2025-05-20 20:21:42 +0300

    qrrrrrrrrr

--- visualbench/tasks/linalg/decompositions.py
+++ visualbench/tasks/linalg/decompositions.py
@@ -8,10 +8,12 @@ from . import linalg_utils
 
 
 class QR(Benchmark):
+    """QR, this doesn't force R diagonal to be positive"""
     def __init__(
         self,
         A,
-        ortho: linalg_utils.OrthoMode = 1,
+        ortho: linalg_utils.OrthoMode = 100,
+        exp_diag: bool = True,
         mode: Literal["full", "reduced"] = "reduced",
         criterion=torch.nn.functional.mse_loss,
         algebra=None,
@@ -24,35 +26,41 @@ class QR(Benchmark):
         self.criterion = criterion
         self.ortho: linalg_utils.OrthoMode = ortho
         self.algebra = algebras.get_algebra(algebra)
+        self.exp_diag = exp_diag
 
         *b, m, n = self.A.shape
         k = min(m, n) if mode == 'reduced' else m
-        self.Q = torch.nn.Parameter(torch.zeros(*b, m, k))
+        self.Q = torch.nn.Parameter(torch.nn.init.orthogonal_(torch.zeros(*b, m, k), generator=self.rng.torch()))
         self.R = torch.nn.Parameter(torch.zeros(*b, k, n))
 
         self.add_reference_image('A', self.A, to_uint8=True)
-        try:
-            Q, R = torch.linalg.qr(self.A, mode=mode) # pylint:disable=not-callable
-            self.add_reference_image('true Q', Q, to_uint8=True)
-            self.add_reference_image('true R', R, to_uint8=True)
-        except torch.linalg.LinAlgError as e:
-            warnings.warn(f'true QR failed for some reason: {e!r}')
+        if algebra is None:
+            try:
+                Q, R = torch.linalg.qr(self.A, mode=mode) # pylint:disable=not-callable
+                self.add_reference_image('true Q', Q, to_uint8=True)
+                self.add_reference_image('true R', R, to_uint8=True)
+            except torch.linalg.LinAlgError as e:
+                warnings.warn(f'true QR failed for some reason: {e!r}')
 
 
     def get_loss(self):
         A = self.A
         Q = self.Q
-        R = torch.triu(self.R)
 
-        print(f'{Q.shape = }, {R.shape = }')
-        QR_ = algebras.matmul(Q, R, self.algebra)
-        loss = self.criterion(QR_, A)
+        if self.exp_diag:
+            R = torch.triu(self.R, diagonal=1)
+            R = R + self.R.diagonal(dim1=-2, dim2=-1).exp().diag_embed()
+        else:
+            R = torch.triu(self.R)
 
         Q, penalty = linalg_utils.orthonormality_constraint(Q, ortho=self.ortho, algebra=self.algebra, criterion=self.criterion)
 
+        QR_ = algebras.matmul(Q, R, self.algebra)
+        loss = self.criterion(QR_, A)
+
         if self._make_images:
             self.log_image("Q", self.Q, to_uint8=True, log_difference=True)
-            self.log_image("R", self.R, to_uint8=True, log_difference=True)
+            self.log_image("R", R, to_uint8=True, log_difference=True)
             self.log_image("QR", QR_, to_uint8=True, show_best=True)
 
         return loss + penalty
--- visualbench/tasks/linalg/linalg_utils.py
+++ visualbench/tasks/linalg/linalg_utils.py
@@ -4,14 +4,29 @@ import torch
 from torch.nn import functional as F
 from ...utils import algebras
 
+
+
+def svd(M, driver=None):
+    device=M.device
+    if driver is not None: M = M.cuda()
+    U, S, V = torch.linalg.svd(M, driver=driver) #pylint:disable=not-callable
+    if driver is not None: return U.to(device), S.to(device), V.to(device)
+    return U, S, V
+
+def orthonormalize_svd(M, driver=None):
+    U,S,V = svd(M, driver)
+    return (U @ V.mT)
+
 OrthoMode = float | Literal['svd']
 def orthonormality_constraint(M: torch.Tensor, ortho: OrthoMode, algebra, criterion=F.mse_loss):
     """either orthonormality penalty or projects onto the Stiefel manifold via svd"""
     if not isinstance(M, torch.Tensor): raise TypeError(M)
 
     if ortho == 'svd':
-        U, S, V = torch.linalg.svd(M) #pylint:disable=not-callable
-        return U @ V.T, 0
+        try:
+            return orthonormalize_svd(M), 0
+        except torch.linalg.LinAlgError:
+            ortho = 1
 
     *b, m, n = M.shape
     if n > m:
@@ -19,6 +34,6 @@ def orthonormality_constraint(M: torch.Tensor, ortho: OrthoMode, algebra, criter
         m, n = n, m
 
     I = torch.eye(n, dtype=M.dtype, device=M.device).expand(M.shape).clone()
-    penalty = criterion(algebras.matmul(M.T, M, algebra=algebra), I)
+    penalty = criterion(algebras.matmul(M.mT, M, algebra=algebra), I)
 
     return M, penalty*ortho
